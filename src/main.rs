6182ea8bde32859564fd559b9b8740884a363928
Darrion Burgess <dargondab9@gmail.com>
2022-01-13 22:16:59 -0600

    first commit of habit tracker, currently I can add to the list

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+fn add_habit(habit_list: Vec<String>, habit: &str) -> Vec<String> {
+    let mut updated_habit_list = habit_list.clone();
+    updated_habit_list.push(habit.to_string());
+    return updated_habit_list;
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[test]
+fn add_basic_habit() {
+    let test_habit_list = add_habit(vec![], "test habit");
+    assert!(test_habit_list[0] == "test habit")
+}
+
+#[test]
+fn add_two_habits() {
+    let habit_list = add_habit(vec![], "first habit");
+    let habit_list = add_habit(habit_list, "second habit");
+
+    assert!(habit_list[0] == "first habit" && habit_list[1] == "second habit");
+}
